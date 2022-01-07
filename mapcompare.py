a2b582cd956363aa2cc011d23a9647bd2741b0fd
Scott Gallacher <61019982+scottgallacher-3@users.noreply.github.com>
2022-01-07 16:13:13 +0000

    Upgraded `comparebonds` to use `pandas` DataFrame
    Additional formatting improvements

--- mapcompare.py
+++ mapcompare.py
@@ -12,12 +12,13 @@ import matplotlib.pyplot as plt
 import scipy.optimize
 import scipy.ndimage
 from itertools import combinations
+import pandas as pd
 
 from mapobject import ZygoMap
 from utilities import matchdims, ztestf
 
 
-def combinemaps(lowermap, uppermap, optimised=True, output=True):
+def combinemaps(lowermap, uppermap, newname=None, optimised=True, output=True):
     m1,m2 = lowermap.heights, uppermap.heights
     #function to combine ZygoMap objects
     #flips and negates values of the 2nd map "uppermap"
@@ -59,10 +60,13 @@ def combinemaps(lowermap, uppermap, optimised=True, output=True):
     #construct as ZygoMap object
     #providing basic details about its construction (the combination of which maps at what angle)
     if hasattr(lowermap, "mapname") and hasattr(uppermap, "mapname"):
-        interfacemap = ZygoMap(array=interface, map1=lowermap, map2=uppermap, angle=optimalangle,
-                               mapname="{0}+{1}".format(lowermap.mapname, uppermap.mapname))
+        if newname is None:
+            newname = "{0}+{1}".format(lowermap.mapname, uppermap.mapname)
+            
+        interfacemap = ZygoMap(array=interface, map1=lowermap, map2=uppermap, angle=optimalangle, mapname=newname)
+        
     else:
-        interfacemap = ZygoMap(array=interface)
+        interfacemap = ZygoMap(array=interface, mapname=newname)
     
     #give user some knowledge on the optimised set-up
     #this could be moved elsewhere possibly for better access ->  added __str__() method to allow user to print() attributes
@@ -70,11 +74,12 @@ def combinemaps(lowermap, uppermap, optimised=True, output=True):
     #set output to false for auto-comparison, so can display at end in a sorted order
     if optimised and output:
         if getattr(lowermap, "mapname") and getattr(uppermap, "mapname"):  # - if each defined from files, can use their filenames as references
-            print("Maps combined for optimal angle of {0:.2f} degrees\n\
-            {1} clockwise w.r.t {2}".format(optimalangle,uppermap.mapname,lowermap.mapname))
+            print(("Combined interface map: '{0}'"
+                   "\nMaps combined for optimal angle of {1:.2f} degrees."
+                   "\n'{2}' clockwise w.r.t '{3}'").format(interfacemap.mapname, optimalangle,uppermap.mapname,lowermap.mapname))
         else:
-            print("Maps combined for optimal angle of {0:.2f} degrees\n\
-            (map2 clockwise w.r.t map1)".format(optimalangle))
+            print(("Maps combined for optimal angle of {0:.2f} degrees"
+                   "map2 clockwise w.r.t map1").format(optimalangle))
         print("Optimised peak-to-valley height: {0:.1f} nm".format(interfacemap.peakvalley * 1e9))
         print("Optimised RMS height: {0:.1f} nm".format(interfacemap.rms * 1e9))
         
@@ -82,101 +87,72 @@ def combinemaps(lowermap, uppermap, optimised=True, output=True):
     return interfacemap
 
 
-def comparebonds(zmaps, sort="both", print_output=False, plot=False):
-    #NOTE: using itertools.combinations module
-    #for a list (or maybe dict as well ?) of map objects, iterate through every pair combination
-    #comparing bonds by peak-to-valley height & RMS height
-    #additionally, can print information about optimal angle (around z-axis) to combine each pair
-    
-    #attain array from generator object returned by combinations (using n=2 items per combo)
-    mpairs = np.array(list(combinations(zmaps, 2)))
+def comparebonds(zmaps, plot=False):
+    if isinstance(zmaps, dict):
+        mpairs = np.array(list(combinations(zmaps.values(), 2)))
+    elif isinstance(zmaps, list) or isinstance(zmaps, tuple):
+        mpairs = np.array(list(combinations(zmaps, 2)))
+    else:
+        raise TypeError(("'comparebonds' function requires an iterable of type 'dict', 'list', or 'tuple'. "
+                         "The iterable must contain only objects of type 'ZygoMap'"))
     
     #combine maps, creating interface object for each pair (of ZygoMap class, defined with array rather than filename)
     #will store the PV & RMS values for each in numpy arrays, to then be sorted best to worst
-    #choice to store the interfaces ? - for low number of arrays this should be ok
-    
-    #initialise empty arrays which will store the PV/RMS as calculated (necessary if not storing each interface)
-    pvValues = np.zeros(len(mpairs))
-    rmsValues = np.zeros(len(mpairs))
     
     interfacemaps = np.zeros(len(mpairs), dtype=object)  # - to store each combo pair's interfacemap (ZygoMap object)
-    #note: may not be so simple - only local to function, may need to either return interfacemaps or create as global variable
     for i in range(len(mpairs)):
         combo = mpairs[i]
         interfacemap = combinemaps(*combo, output=False)
-        pvValues[i] = interfacemap.peakvalley
-        rmsValues[i] = interfacemap.rms
-        
         interfacemaps[i] = interfacemap
         
-    #now, sort for display
-    #sort the mpairs list differently for either pv or rms, storing each separately
-    #allow "sort" keyword to limit comparison to only pv or only rms (defaults to "both")
-    lowestpv = mpairs[pvValues.argsort()]
-    lowestrms = mpairs[rmsValues.argsort()]
+    #create a pandas DataFrame to concentrate relevant quantities/attributes to one object
+    #there is plenty of scope to use the data further, so best to provide maps and metrics alongside
+    #provide: the interface 'ZygoMap' objects ["Bond Map"],
+    #names of combined maps ["map1"],["map2"],
+    #peakvalley ["PV"] and rms ["RMS"] heights.
+    sorted_bonds = pd.DataFrame({"Bond Map": interfacemaps,
+                                 "map1": [bond.map1.mapname for bond in interfacemaps], 
+                                 "map2": [bond.map2.mapname for bond in interfacemaps], 
+                                 "PV": [bond.peakvalley for bond in interfacemaps], 
+                                 "RMS": [bond.rms for bond in interfacemaps]})
     
-    #maps constructed between pairs, and ordered
-    #this can be returned
-    pv_sorted_maps = interfacemaps[pvValues.argsort()]
-    rms_sorted_maps = interfacemaps[rmsValues.argsort()]
-    
-    #sort combos by lowest peak-valley height
-    if sort in ("both","pv"):
-        if print_output:
-            print("Sample Bonds sorted by lowest peak-to-valley height:\n")
-        for i in range(len(lowestpv)):
-            combo = lowestpv[i]
-            if print_output:
-                print("{} - ".format(i+1),*[m.filename for m in combo])
-                print("Peak-to-valley Height: {0:.1f} nm".format(pvValues[pvValues.argsort()][i] * 1e9))  # - convert to nanometres
-                print("RMS Height: {0:.1f} nm".format(rmsValues[pvValues.argsort()][i] * 1e9))
-                print("\n")
-    
-    #sort combos by lowest rms height
-    if sort in ("both","rms"):
-        if print_output:
-            print("Sample Bonds sorted by lowest RMS height:\n")
-        for i in range(len(lowestrms)):
-            combo = lowestrms[i]
-            if print_output:
-                print("{} - ".format(i+1),*[m.filename for m in combo])
-                print("RMS Height: {0:.1f} nm".format(rmsValues[rmsValues.argsort()][i] * 1e9))
-                print("Peak-to-valley Height: {0:.1f} nm".format(pvValues[rmsValues.argsort()][i] *1e9))
-                print("\n")
+    #sort by peakvalley as standard
+    sorted_bonds = sorted_bonds.sort_values(by="PV")
             
     if plot is True:
-        if sort in ("both","pv"):
-            #sort the found pv & rms heights, ordering by the lowest pv in both cases (keep both values aligned per combo)
-            plt.figure(figsize=(10,6))
-            plt.plot(pvValues[pvValues.argsort()], "o", label="Peak-Valley")
-            plt.plot(rmsValues[pvValues.argsort()], "o", label="RMS")
-            plt.title("Bond height values comparison (sorted by lowest peak-valley value)\n")
+        #sort the found pv & rms heights, ordering by the lowest pv in both cases (keep both values aligned per combo)
+        plt.figure(figsize=(12,5))
+        plt.plot(sorted_bonds.sort_values(by="PV")["PV"].values, "o", label="Peak-Valley")
+        plt.plot(sorted_bonds.sort_values(by="PV")["RMS"].values, "o", label="RMS")
+        plt.title("Bond height values comparison (sorted by lowest peak-valley value)\n")
+
+        plt.xlabel("Maps comprising simulated bond")
+        plt.ylabel("Height of bond interface (nm)")
+        plt.xticks(range(len(mpairs)), np.array([bond.mapname for bond in sorted_bonds.sort_values(by="PV")["Bond Map"]]), rotation=50)
+        plt.yticks(np.arange(0, sorted_bonds["PV"].max() + 10e-9, 10e-9))
+        plt.ticklabel_format(axis="y", style="sci", scilimits=(-9,-9), useMathText=True)
+        plt.tick_params(axis="x", labelsize=6)
+        plt.axhline(60e-9, linestyle="solid", linewidth=3, color="r")
+        plt.legend(loc="upper left", fontsize=14)
+        plt.grid()
+        plt.show()
             
-            plt.xlabel("Maps used in simulated bond")
-            plt.ylabel("Height of bond interface (nm)")
-            plt.xticks(range(len(mpairs)), np.array([m.map1.split(".")[0]+"\n"+m.map2.split(".")[0] for m in interfacemaps])[pvValues.argsort()], rotation=0)
-            plt.yticks(np.arange(0, max(pvValues) + 10e-9, 10e-9))
-            plt.ticklabel_format(axis="y", style="sci", scilimits=(-9,-9), useMathText=True)
-            plt.axhline(60e-9, linestyle="solid", linewidth=3, color="r")
-            plt.legend(loc="upper left", fontsize=14)
-            plt.grid()
-            plt.show()
             
-        if sort in ("both","rms"):
-            #sort the found pv & rms heights, ordering by the lowest rms in both cases (keep both values aligned per combo)
-            plt.figure(figsize=(10,6))
-            plt.plot(pvValues[rmsValues.argsort()], "o", label="Peak-Valley")
-            plt.plot(rmsValues[rmsValues.argsort()], "o", label="RMS")
-            plt.title("Bond height values comparison (sorted by lowest RMS value)\n")
+        #sort the found pv & rms heights, ordering by the lowest rms in both cases (keep both values aligned per combo)
+        plt.figure(figsize=(12,5))
+        plt.plot(sorted_bonds.sort_values(by="RMS")["PV"].values, "o", label="Peak-Valley")
+        plt.plot(sorted_bonds.sort_values(by="RMS")["RMS"].values, "o", label="RMS")
+        plt.title("Bond height values comparison (sorted by lowest RMS value)\n")
+
+        plt.xlabel("Maps comprising simulated bond")
+        plt.ylabel("Height of bond interface (nm)")
+        plt.xticks(range(len(mpairs)), np.array([bond.mapname for bond in sorted_bonds.sort_values(by="RMS")["Bond Map"]]), rotation=50)
+        plt.yticks(np.arange(0, sorted_bonds["PV"].max() + 10e-9, 10e-9))
+        plt.ticklabel_format(axis="y", style="sci", scilimits=(-9,-9), useMathText=True)
+        plt.tick_params(axis="x", labelsize=6)
+        plt.axhline(60e-9, linestyle="solid", linewidth=3, color="r")
+        plt.legend(loc="upper left", fontsize=14)
+        plt.grid()
+        plt.show()
             
-            plt.xlabel("Maps used in simulated bond")
-            plt.ylabel("Height of bond interface (nm)")
-            plt.xticks(range(len(mpairs)), np.array([m.map1.split(".")[0]+"\n"+m.map2.split(".")[0] for m in interfacemaps])[rmsValues.argsort()], rotation=0)
-            plt.yticks(np.arange(0, max(pvValues) + 10e-9, 10e-9))
-            plt.ticklabel_format(axis="y", style="sci", scilimits=(-9,-9), useMathText=True)
-            plt.axhline(60e-9, linestyle="solid", linewidth=3, color="r")
-            plt.legend(loc="upper left", fontsize=14)
-            plt.grid()
-            plt.show()
-            
-    return pv_sorted_maps, rms_sorted_maps  # - nothing right now (simplest for usability) but could offer the sorted arrays and all interfacemaps
+    return sorted_bonds
